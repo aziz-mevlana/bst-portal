567e8585d0f25730e6a33f40cbada0a33fd78566
Aziz Mevlana Alim <147847410+aziz-mevlana@users.noreply.github.com>
2025-05-22 12:59:11 +0300

    add alumni filter&search

--- alumni/views.py
+++ alumni/views.py
@@ -2,13 +2,45 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import AlumniProfile, AlumniExperience, Tag
+from django.db import models
 
 def alumni_list(request):
     alumni_list = AlumniProfile.objects.all()
     tags = Tag.objects.all()
+
+    # Arama ve filtreleme parametreleri
+    query = request.GET.get('q', '')
+    experience_level = request.GET.get('experience_level', '')
+    graduation_year = request.GET.get('graduation_year', '')
+    tag_id = request.GET.get('tag', '')
+
+    if query:
+        alumni_list = alumni_list.filter(
+            models.Q(user__username__icontains=query) |
+            models.Q(user__first_name__icontains=query) |
+            models.Q(user__last_name__icontains=query) |
+            models.Q(current_position__icontains=query) |
+            models.Q(company__icontains=query) |
+            models.Q(bio__icontains=query)
+        )
+    if experience_level:
+        alumni_list = alumni_list.filter(experience_level=experience_level)
+    if graduation_year:
+        alumni_list = alumni_list.filter(graduation_year=graduation_year)
+    if tag_id:
+        alumni_list = alumni_list.filter(tags__id=tag_id)
+
+    # Mezuniyet yıllarını unique olarak al
+    graduation_years = AlumniProfile.objects.values_list('graduation_year', flat=True).distinct().order_by('-graduation_year')
+
     return render(request, 'alumni/alumni_list.html', {
         'alumni_list': alumni_list,
-        'tags': tags
+        'tags': tags,
+        'graduation_years': graduation_years,
+        'selected_experience_level': experience_level,
+        'selected_graduation_year': graduation_year,
+        'selected_tag': tag_id,
+        'query': query,
     })
 
 def alumni_detail(request, alumni_id):
--- bst_portal/settings.py
+++ bst_portal/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'alumni',
     'accounts',
     'news',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
