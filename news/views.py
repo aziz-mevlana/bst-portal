8d3904ae1875cb5e59d7897406a28ae09460e6a8
Aziz Mevlana Alim <147847410+aziz-mevlana@users.noreply.github.com>
2025-05-27 16:51:59 +0300

    add event detail, news detail, fix css

--- news/views.py
+++ news/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import News
 
-def index(request):
-    news = News.objects.all()[:3]
-    return render(request, 'index.html', {'news': news})
 
 def news_list(request):
     news = News.objects.all()
